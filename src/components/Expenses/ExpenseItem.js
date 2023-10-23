7faeeeb106254aac7cad3c05644898df3c506673
SAM ADKINS <sjamesadkins@gmail.com>
2023-10-23 18:25:42 -0500

    React tutorial complete through Module 79

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
+import React, { useState } from 'react';
+
+
 
 function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+  
+  const clickHandler = () => {
+    setTitle('Updated!')
+    console.log(title)
+  };
+  
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Item</button>
     </Card>
   );
 }
